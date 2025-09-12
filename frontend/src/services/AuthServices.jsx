4b447855d7cdfc6c2d46cbdb199220b80a63b918
Rahul Yadav <rahul271435@gmail.com>
2025-09-12 21:07:53 +0530

    updated

--- frontend/src/services/AuthServices.jsx
+++ frontend/src/services/AuthServices.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8080/api"
+    : import.meta.env.VITE_API_BASE_URL;
 
 const registerUser = (data) => {
   return axios.post(`${API_BASE_URL}/api/user/register`, data);
