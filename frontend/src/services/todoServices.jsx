ab7e1ca18366bcadbff913161cad66305f86e3ff
Rahul Yadav <rahul271435@gmail.com>
2025-09-12 20:55:04 +0530

    updated

--- frontend/src/services/todoServices.jsx
+++ frontend/src/services/todoServices.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8080/api"
+    : import.meta.env.VITE_API_BASE_URL;
 
 //get user token
 const user = JSON.parse(localStorage.getItem("todoapp")) || {};
